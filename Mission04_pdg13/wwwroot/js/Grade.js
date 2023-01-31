f362592acdfa8986091a40080cc480b28decc1d7
parkergeorge13 <parkergeorge13@gmail.com>
2023-01-30 21:15:09 -0700

    Add project files.

---
+++ Mission04_pdg13/wwwroot/js/Grade.js
@@ -0,0 +1,48 @@
+﻿$("#btnSend").click(function () {
+    var finalGrade = ($("#txtAssignments").val() * .5) + ($("#txtProject").val() * .1) + ($("#txtQuizzes").val() * .1)
+        + ($("#txtMidExam").val() * .1) + ($("#txtFinalExam").val() * .1) + ($("#txtIntex").val() * .1)
+    var finalGradeP = finalGrade + '%'
+    var finalGradeL
+
+    switch (true) {
+        case finalGrade >= 94:
+            finalGradeL = 'A'
+            break;
+        case finalGrade >= 90:
+            finalGradeL = 'A-'
+            break;
+        case finalGrade >= 87:
+            finalGradeL = 'B+'
+            break;
+        case finalGrade >= 84:
+            finalGradeL = 'B'
+            break;
+        case finalGrade >= 80:
+            finalGradeL = 'B-'
+            break;
+        case finalGrade >= 77:
+            finalGradeL = 'C+'
+            break;
+        case finalGrade >= 74:
+            finalGradeL = 'C'
+            break;
+        case finalGrade >= 70:
+            finalGradeL = 'C-'
+            break;
+        case finalGrade >= 67:
+            finalGradeL = 'D+'
+            break;
+        case finalGrade >= 64:
+            finalGradeL = 'D'
+            break;
+        case finalGrade >= 60:
+            finalGradeL = 'D-'
+            break;
+        case finalGrade < 60:
+            finalGradeL = 'E'
+            break;
+    }
+    $("#GradePercent").html(finalGradeP)
+    $("#GradeLetter").html(finalGradeL)
+
+})
